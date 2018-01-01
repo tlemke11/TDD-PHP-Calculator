569064f924aa57079d396b1cacad21f54935af44
Tyler lemke <tylerlemke@Tylers-MacBook-Pro.local>
2018-01-01 10:15:57 -0600

    php unit and composer installed

---
+++ CalculatorTest.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: tylerlemke
+ * Date: 1/1/18
+ * Time: 10:06 AM
+ */
+
+
+class CalculatorTest extends PHPUnit_Framework_TestCase {
+
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+To get started with PHP Unit in PHPStorm please follow these instructions:
+https://www.jetbrains.com/help/phpstorm/testing-with-phpunit.html
